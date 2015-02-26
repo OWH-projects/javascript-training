7b949cae1b81ec72bdee59f80f8a0535d850d00c
cjwinchester <cjwinchester@gmail.com>
2015-02-25 23:45:35 -0600

    no it's _fine_ yes i know how to spell hexidecimal

--- examples/data/journos.js
+++ examples/data/journos.js
@@ -13,14 +13,14 @@ var journo = [
         "name": "Graham Archer",
         "section": "Sports",
         "favorite_color": "purple"
-    },
+    }
     /*
     
     add more people here and then reload the page to see what happens
     
     pick the color names from here: http://www.w3schools.com/html/html_colornames.asp
 
-    ... or use hexadecminal codes: http://www.w3schools.com/tags/ref_colorpicker.asp    
+    ... or use hexadecimal codes: http://www.w3schools.com/tags/ref_colorpicker.asp    
 
     */
 ]
